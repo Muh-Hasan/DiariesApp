0535f94c3d28102772428f0b1dd1b39eeb6f9b85
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-02 06:10:12 -0700

    store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
-import { configureStore , getDefaultMiddleware  } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+import { useDispatch } from 'react-redux';
 
-export const store = configureStore({
-    reducer: rootReducer,
-    middleware: [...getDefaultMiddleware(), customMiddleware],
+const store = configureStore({
+  reducer: rootReducer,
 });
+
+type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export default store;
